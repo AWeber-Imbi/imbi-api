591bdaea62ccf26a2f56c70442084e79dec86dcd
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2024-03-04 14:10:18 -0500

    Add optional aws_ssm_slug to namespaces
    This allows users to specify a custom namespace slug to be templated
    into the AWS SSM path prefix for configuration params for a project.

--- imbi/endpoints/namespaces.py
+++ imbi/endpoints/namespaces.py
@@ -8,7 +8,7 @@ class _RequestHandlerMixin:
     ID_KEY = 'id'
     FIELDS = [
         'id', 'name', 'slug', 'icon_class', 'maintained_by',
-        'gitlab_group_name', 'sentry_team_slug'
+        'gitlab_group_name', 'sentry_team_slug', 'aws_ssm_slug'
     ]
     DEFAULTS = {'icon_class': 'fas fa-users', 'maintained_by': []}
 
@@ -17,7 +17,8 @@ class _RequestHandlerMixin:
         SELECT id, "name", created_at, created_by,
                last_modified_at, last_modified_by,
                slug, icon_class, maintained_by,
-               gitlab_group_name, sentry_team_slug
+               gitlab_group_name, sentry_team_slug,
+               aws_ssm_slug
           FROM v1.namespaces WHERE id=%(id)s""")
 
 
@@ -30,17 +31,17 @@ class CollectionRequestHandler(_RequestHandlerMixin,
     COLLECTION_SQL = re.sub(
         r'\s+', ' ', """ \
         SELECT id, "name", slug, icon_class, maintained_by, gitlab_group_name,
-               sentry_team_slug
+               sentry_team_slug, aws_ssm_slug
           FROM v1.namespaces ORDER BY "name" ASC""")
 
     POST_SQL = re.sub(
         r'\s+', ' ', """\
         INSERT INTO v1.namespaces
                     ("name", created_by, slug, icon_class, "maintained_by",
-                     gitlab_group_name, sentry_team_slug)
+                     gitlab_group_name, sentry_team_slug, aws_ssm_slug)
              VALUES (%(name)s, %(username)s, %(slug)s, %(icon_class)s,
                      %(maintained_by)s, %(gitlab_group_name)s,
-                     %(sentry_team_slug)s)
+                     %(sentry_team_slug)s, %(aws_ssm_slug)s)
           RETURNING id""")
 
 
@@ -61,4 +62,5 @@ class RecordRequestHandler(_RequestHandlerMixin, base.AdminCRUDRequestHandler):
                "maintained_by" = %(maintained_by)s,
                gitlab_group_name = %(gitlab_group_name)s,
                sentry_team_slug = %(sentry_team_slug)s
+               aws_ssm_slug = %(aws_ssm_slug)s
          WHERE id=%(id)s""")
