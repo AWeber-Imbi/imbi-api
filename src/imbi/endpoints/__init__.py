90d8e036ec7dfbc72a102638a4d7492ab7da9eff
Gavin M. Roy <gavinr@aweber.com>
2025-12-30 13:32:58 -0500

    Reorder routers to prioritize blueprint_router initialization

--- src/imbi/endpoints/__init__.py
+++ src/imbi/endpoints/__init__.py
@@ -3,6 +3,6 @@ import fastapi
 from .blueprints import blueprint_router
 from .status import status_router
 
-routers: list[fastapi.APIRouter] = [status_router, blueprint_router]
+routers: list[fastapi.APIRouter] = [blueprint_router, status_router]
 
 __all__ = ['routers']
